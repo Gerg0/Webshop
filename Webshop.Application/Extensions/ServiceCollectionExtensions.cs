6fed37b96f41dcaaaa24b7f1bc021019128ad36c
Gerg0 <tothgergo602@gmail.com>
2024-12-25 15:23:56 +0100

    Added automapper and logging abstractions packages, added mediatr, fluentvalidation and automapper to service collection

--- Webshop.Application/Extensions/ServiceCollectionExtensions.cs
+++ Webshop.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
 using Webshop.Application.Users;
 
 namespace Webshop.Application.Extensions;
@@ -6,6 +8,10 @@ public static class ServiceCollectionExtensions
 {
     public static void AddApplication(this IServiceCollection services)
     {
+        var applicationAssembly = typeof(ServiceCollectionExtensions).Assembly;
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(applicationAssembly));
+        services.AddAutoMapper(applicationAssembly);
+        services.AddValidatorsFromAssembly(applicationAssembly).AddFluentValidationAutoValidation();
         services.AddScoped<IUserContext, UserContext>();
         services.AddHttpContextAccessor();
     }
